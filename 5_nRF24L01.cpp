6896750a115231b18c38cdf055ea59796911f21c
Negin <neginkheirmand@gmail.com>
2020-10-25 15:42:24 +0330

    created nrf.cpp file

---
+++ 5_nRF24L01.cpp
@@ -0,0 +1,13 @@
+
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <RF24/RF24.h>
+
+using namespace std;
+
+int main(int argc, char**argv){
+	cout<<"hi";
+}
